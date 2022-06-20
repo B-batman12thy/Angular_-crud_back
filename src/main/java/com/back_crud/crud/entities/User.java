f5458a36110351d4a4a15a93d6941417585ab8d5
djiby thioub <thioubdjiby96@gmail.com>
2022-06-20 12:20:48 +0000

    utilisateur_entity

--- src/main/java/com/back_crud/crud/entities/User.java
+++ src/main/java/com/back_crud/crud/entities/User.java
@@ -1,7 +1,16 @@
 package com.back_crud.crud.entities;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
+@Getter
+@Data
+@Setter
+@NoArgsConstructor
 @Entity
 public class User implements Serializable {
     @Id
@@ -9,11 +18,17 @@ public class User implements Serializable {
     @Column(name = "id")
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name ="nom", length = 30)
+    private String nom;
+    @Column(name="prenom",length = 30)
+    private String prenom;
+    @Column(name="email", length = 30)
+    private String email;
+    @Column(name="pays", length = 20)
+    private String pays;
+    @Column(name="ville", length = 20)
+    private String ville;
+    @Column (name="telephone", length = 20)
+    private String telephone;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
